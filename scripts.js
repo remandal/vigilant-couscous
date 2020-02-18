1e243efd9e056586d0ac96bf49e32ab8723321ad
remandal <reetammandal@rediffmail.com>
2020-02-19 02:45:35 +0530

    completed the tutorial.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+
+
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+
+
+
+
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()      
 
@@ -10,17 +28,32 @@ request.onload = function() {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-        // Log each movie's title
-        // Create a div with a card class
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-          // Create an h1 and set the text content to the film's title
-        const h1 = document.createElement('h1')
-        h1.textContent = movie.title
+      // Create a div with a card class
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      // Create an h1 and set the text content to the film's title
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      // Create a p and set the text content to the film's description
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300) // Limit to 300 chars
+      p.textContent = `${movie.description}...` // End with an ellipses
+
+      // Append the cards to the container element
+      container.appendChild(card)
+
+      // Each card will contain an h1 and a p
+      card.appendChild(h1)
+      card.appendChild(p)
       console.log(movie.title)
       console.log(movie.description)
     })
   } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
     console.log('error')
   }
 }
@@ -41,13 +74,4 @@ request.send()
 
 
 
-const app = document.getElementById('root')
 
-const logo = document.createElement('img')
-logo.src = 'logo.png'
-
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(logo)
-app.appendChild(container)
